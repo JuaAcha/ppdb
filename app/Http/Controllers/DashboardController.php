04591c53d64827cc2f6c037fc23ec4fc6afd1b0f
JuaAcha <achilhamm.a@gmail.com>
2022-11-29 09:12:15 +0700

    Dashboard Total Data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Jurusan;
+use App\Models\Siswa;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $jurusan = Jurusan::all();
+        $siswa = Siswa::all();
         $user = User::all();
-        return view('dashboard.index', compact('user'));
+        return view('dashboard.index', compact('user', 'jurusan', 'siswa'));
     }
 }
